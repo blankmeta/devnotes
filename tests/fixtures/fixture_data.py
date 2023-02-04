103eb667b493619438845bbe00c11908778c09a6
blankmeta <yadanilfilatov@gmail.com>
2023-02-04 14:29:22 +0300

    Coverage

--- tests/fixtures/fixture_data.py
+++ tests/fixtures/fixture_data.py
@@ -1,9 +1,9 @@
 import pytest
 
-from notes.models import Theme
-
 
 @pytest.fixture
 def create_users_themes(user):
+    from notes.models import Theme
+
     another_theme = Theme.objects.create(title='some_title')
     another_theme.author.add(user.id)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,32 @@
+from rest_framework import status
+
+
 class TestApi:
     PREFIX = '/api/v1/'
+    BASE_URL = PREFIX + 'themes/'
 
-    def test_get_notes(self, user_client, create_users_themes):
-        """Аутентифицированный пользователь может получить свои темы"""
-        url = self.PREFIX + 'themes/'
-        response = user_client.get(url)
-        assert response.data[0]['title'] == 'some_title'
+    def test_non_auth_get_themes(self, client):
+        """Неаутентифицированный пользователь не может получить свой список тем."""
+        response = client.get(self.BASE_URL)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED, (
+            'Неаутентифицированный пользователь должен получать 401_UNAUTHORIZED при попытке получить список своих тем.'
+        )
+
+    def test_success_get_themes(self, user_client):
+        """Аутентифицированный пользователь получает 200 при получении списка тем."""
+        response = user_client.get(self.BASE_URL)
+
+        assert response.status_code == status.HTTP_200_OK, (
+            'Аутентифицированный пользователь должен получать 200_OK при запросе списка тем.'
+        )
+
+    def test_get_themes(self, user_client, create_users_themes):
+        """Аутентифицированный пользователь может получить свои темы."""
+        response = user_client.get(self.BASE_URL)
+
+        test_data = response.json()[0]
+
+        assert test_data['title'] == 'some_title', (
+            'Аутентифицированный пользователь получает список своих тем.'
+        )
